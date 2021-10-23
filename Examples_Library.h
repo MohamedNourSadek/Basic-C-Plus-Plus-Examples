ae45c0a4050440f7a97fb72d22aec71eadcf4072
Mohamed Nour <mohamednoursadek@gmail.com>
2021-10-23 17:40:45 +0200

    Update Examples_Library.h

--- Examples_Library.h
+++ Examples_Library.h
@@ -3,24 +3,20 @@
 #include <string>
 #include <vector>
 
-using std::cin;
-using std::cout;
-using std::string;
-using std::vector;
- 
+using namespace std;
 
 // Simple recieve two numbers and output the sum s
 void SimpleIO()
 {
-	std::cout << "Give me two numbers";
-	std::cout << std::endl;
+	cout << "Give me two numbers";
+	cout <<  endl;
 
 	int x = 0;
 	int y = 0;
 
-	std::cin >> x;
-	std::cin >> y;
-	std::cout << "The sum is : " << x + y;
+	cin >> x;
+	cin >> y;
+	cout << "The sum is : " << x + y;
 }
 
 // While app (sum numbers from 50 to 100)
@@ -33,31 +29,32 @@ void While_App1()
 		sum += y;
 		y++;
 	}
-	std::cout << "sum is : " << sum;
+	cout << "sum is : " << sum;
 }
 
 //While App (Show Numbers between a range entered by the user
 void While_App2()
 {
-	std::cout << "Enter the first number: ";
+	cout << "Enter the first number: ";
 	int y = 0; int z = 0;
-	std::cin >> y;
-	std::cout << "Enter the second number : ";
-	std::cin >> z;
+	cin >> y;
+	cout << "Enter the second number : ";
+	cin >> z;
 	int range = z - y;
 	while (range >= 0)
 	{
-		std::cout << "Number is: " << z - range << "\n";
+		cout << "Number is: " << z - range << "\n";
 		range--;
 	}
 }
 
+//trying For
 void For_App()
 {
 	int sum = 0;
 	for (int y = 0; y <= 3; y++)
 		sum += y;
-	std::cout << sum;
+	cout << sum;
 }
 
 //test loss of data in conversion
@@ -65,69 +62,62 @@ void Data_Conversion()
 {
 	float y = 1.99f;
 	int z = y;
-
-	std::cout << z;
+	cout << z;
 }
 
+//testing edge cases of naming variables
 void WeirdNaming()
 {
 	int _ = 2;
-	std::cout << _;
+	cout << _;
 }
 
+//testing references
 void Using_References()
 {
 	int y = 50;
 	int& z = y; //a reference must be initialized and can't be modified later
 	z = 40;
-
-	std::cout << y;
+	cout << y;
 }
 
+//testing pointers
 void Using_Pointer()
 {
 	int y = 40;
 	int* p;
 	p = &y;
 	*p = 1;
-
-	std::cout << y;
+	cout << y;
 }
 
+//testing the auto type keyword
 void Using_Auto()
 {
 	int i = 5;
 	double j = 3.1;
-
 	auto y = j + i;
-
-	std::cout << y;
+	cout << y;
 }
 
+//testing type detector
 void Discover_type()
 {
 	int i = 1;
 	decltype(i) y = 4;
-
-	std::cout << y;
-
+	cout << y;
 }
 
-struct Test_DataType
-{
-	int x = 0;
-	float y = 2;
-};
-
+//testing the creation of a new structure
+struct Test_DataType {	int x = 0; 	float y = 2;};
 void Testing_a_DataType()
 {
 	Test_DataType tests;
-	std::cin >> tests.y;
-
-	std::cout << "x is : " << tests.x << ", y is : " << tests.y;
-
+	cin >> tests.y;
+	cout << "x is : " << tests.x << ", y is : " << tests.y;
 }
 
+//testing using cout instead of std::cout
 void Test_Using()
 {
 	int x = 0;
@@ -136,43 +126,41 @@ void Test_Using()
 	cout << x;
 }
 
+//string as a array of chars
 void Testing_String()
 {
 	string y = "test text";
 	cout << y;
 }
 
+//testing string functions
 void getLine_String_functiontest()
 {
 	string s;
-	std::getline(cin, s);
-
+	getline(cin, s);
 	if (s.empty())
 		cout << "you didn't enter anything";
 	else
 		for (auto r : s)
 			cout << r << "\n";
-
 	cout << s.size();
 }
 
+//testing vectors
 void Vector_test()
 {
 	vector<int> v = { 1,2,3 };
-
 	v.push_back(4);
-
 	for (int i : v)
 		cout << i << "\n";
-
 	for (int i : v)
 		cout << &i << "\n";
 }
 
+//testing iterators with containers
 void Iterator_Test()
 {
 	vector<int> v = { 1, 2, 3 };
-
 	auto it = v.begin();
 	*it = 13;
 	cout << *it;
@@ -181,6 +169,7 @@ void Iterator_Test()
 
 }
 
+//testing arrays
 void Array_Test()
 {
 	int arr[] = { 1,2,3,4,5 };
@@ -189,6 +178,7 @@ void Array_Test()
 		cout << i << "\n";
 }
 
+//using points to use arrays
 void Array_Pointers()
 {
 	int arr[] = { 1,2,3,4 };
@@ -203,30 +193,28 @@ void Undefined_Expression()
 	cout << i << "," << i++;
 }
 
+//items positions in memory
 void SizeOf_Test()
 {
 	vector<int> i = { 1,2,3 ,4,4,4,5,5,5,5,1,2,3 ,4,4,4,5,5,5,5};
-	
-
 	for (int x = 0; x <= i.size() - 1; ++x)
 		cout << &i[x] << "\n";
-
 	cout << sizeof(i);
 }
 
+//converting between types
 void Explicit_Cat()
 {
 	int i = 3;
 	cout << static_cast<double>(i) / 2;
 }
 
+//testing switch
 void Switch_Test()
 {
 	int i = 0;
 	char x;
 	cin >> x;
-
-
 	switch (x)
 	{
 	case 'i':
@@ -239,10 +227,10 @@ void Switch_Test()
 		i = 10;
 		break;
 	}
-
 	cout << i;
 }
 
+//Custom print functions
 void print(string s)
 {
 	cout << s;
@@ -254,10 +242,13 @@ void negate(int& num)
 	num = -num;
 }
  
+//the concept of function overloading  - f1
 void Overload_x(string s)
 {
 	cout << s;
 }
+
+//the concept of function overloading  - f1
 void Overload_x(int i)
 {
 	cout << i;
